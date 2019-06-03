3d20bdd4e6a3c4038d9d77f954dbb259b26120bb
Edoardo Lenzi <edoardolenzi9@gmail.com>
2019-06-03 10:41:58 +0200

    client

---
+++ client/index.js
@@ -0,0 +1,17 @@
+function CreateCar(){
+    var parameters = {
+        direction: $('#direction > .btn.active').text().trim(),
+        state: $('#state > .btn.active').text().trim(),
+        timer: $('#timer')[0].value
+    }
+    console.dir(parameters);
+}
+
+function HideTimer(){
+    $('#timer-mask').removeClass( "d-block" ).addClass( "d-hide" );
+    $('#timer')[0].value = "";
+}
+
+function ShowTimer(){
+    $('#timer-mask').removeClass( "d-hide" ).addClass( "d-block" );
+}
